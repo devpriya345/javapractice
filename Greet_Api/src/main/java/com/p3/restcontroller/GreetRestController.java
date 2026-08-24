ac30f3be440d412724eac44c43ef2745f6519560
devpriya345 <singhhpriya12345@gmail.com>
2026-08-24 19:38:12 +0530

    priya second push

---
+++ Greet_Api/src/main/java/com/p3/restcontroller/GreetRestController.java
@@ -0,0 +1,25 @@
+package com.p3.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.p3.client.WelcomeApiClient;
+
+@RestController
+public class GreetRestController {
+	
+	@Autowired
+	private WelcomeApiClient apiClient;
+	
+	@GetMapping("/greet")
+	public String getGreetMsg() {
+		
+		String greetResponse = "Good Morning";
+		
+		String welcomeResponse = apiClient.invokeWelcomeApi();
+		
+		return greetResponse + " ," + welcomeResponse;
+		
+	}
+}
